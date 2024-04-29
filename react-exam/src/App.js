e602e9179433b86d40641fd6cfa31548a4ac03f1
Katmore2727 <katmoreyash2121@gmail.com>
2024-04-29 15:08:39 +0530

    Created Home Page

--- react-exam/src/App.js
+++ react-exam/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
 import ImageCarousel from './Components/ImageCarousel'; 
 import Accordion from './Components/Accordion';
-import { Questions } from './Components/Data';
+import { Questions } from './Components/Questions';
+import Quote from './Components/Quote';
+import Search from './Components/Search';
+import Video from './Components/Video';
+import Bmi from './Components/Bmi';
+import Home from './Components/Home';
+import Accordion1 from './Components/Accordion1';
+
 
 function App() {
   return (
     <div className="App">
-     <ImageCarousel></ImageCarousel>
-
-
-     <div className="container1">
-      <h1>Frequently Asked Questions</h1>
-      {Questions.map((element) => (
-        <Accordion
-          key={element.id}
-          question={element.question}
-          answer={element.answer}
-        ></Accordion>
-      ))};
-    </div>
+    <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/image carousel' element={<ImageCarousel />} />
+          <Route path='/accordion' element={<Accordion1 />} />
+          <Route path='/quote' element={<Quote />} />
+          <Route path='/search' element={<Search />} />
+          {/* <Route path='/video' element={<Video />} /> */}
+          <Route path='/bmi' element={<Bmi />} />
+        </Routes>
+    
     </div>
     
   );
---
+++ react-exam/src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar"
+// import './Page1.css'
+
+export default function Home() {
+    return(
+        <div>
+        {/* <Navbar></Navbar> */}
+        <div className="container1">
+            <h1> Mini Projects</h1>
+        </div>
+        </div>
+    );
+} 
