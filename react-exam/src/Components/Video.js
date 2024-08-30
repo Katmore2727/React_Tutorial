82e42a88d52de74373bd4268e48b4fc44888a7b9
Katmore2727 <katmoreyash2121@gmail.com>
2024-08-30 13:19:36 +0530

    updated

---
+++ react-exam/src/Components/Video.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./Navbar"
+
+export default function Video() {
+    return(
+        <>
+        {/* <Navbar></Navbar> */}
+        <h1>This is Video</h1>
+        </>
+    )
+} 
